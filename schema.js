bc99a1e323031d7d8ded11cd845bfcbeafc06aab
Anoop Varghese <anupvarghese@gmail.com>
2016-12-29 20:35:56 +1100

    add schema

---
+++ schema.js
@@ -0,0 +1,21 @@
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+} = require('graphql');
+
+let count = 0;
+
+let schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      count: {
+        type: GraphQLInt,
+        resolve: () => count,
+      },
+    },
+  }),
+});
+
+export default schema;
